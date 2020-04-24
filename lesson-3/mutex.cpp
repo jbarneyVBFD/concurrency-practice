4325cbe5f8335870df070872f9b3fb1ee19bc40a
John Barney <jfbarney1986@yahoo.com>
2020-04-24 17:13:14 -0400

    update files

---
+++ lesson-3/mutex.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <vector>
+#include <algorithm>
+
+class Vehicle
+{
+public:
+    Vehicle(int id) : _id(id){}
+
+private:
+    int _id;
+};
+
+class WaitingVehicles
+{
+public:
+    WaitingVehicles() : _tmpVehicles(0) {}
+
+    //getters and setters
+    void printSize()
+    {
+        std::cout << "#vehicles = " << _tmpVehicles << std::endl;
+    }
+
+    //typical behaviour methods
+    void pushBack(Vehicle &&v)
+    {
+        //_vehicles.push_back(std::move(v)); //data race would cause an exception
+        int oldNum = _tmpVehicles;
+        std::this_thread::sleep_for(std::chrono::microseconds(1)); //wait deliberately to expose data race
+        _tmpVehicles = oldNum + 1;
+    }
+private:
+    int _tmpVehicles;
+    std::vector<Vehicle> _vehicles; //list of all vehicles waiting to enter this intersection 
+};
+
+int main()
+{
+    std::shared_ptr<WaitingVehicles> queue(new WaitingVehicles);
+    std::vector<std::future<void>> futures;
+    int x{10};
+    for (int i =0; i < 1000; ++i)
+    {
+        Vehicle v(i);
+        futures.emplace_back(std::async(std::launch::async, &WaitingVehicles::pushBack, queue, x, std::move(v)));
+    }
+
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr) {
+        ftr.wait();
+    });
+
+    queue->printSize();
+
+    return 0;
+}
